5f576f6556a90904027ffaba54decbb892ce4bea
loitez <87999189+loitez@users.noreply.github.com>
2021-07-27 22:52:51 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -5,53 +5,90 @@ var appButtonRead = document.querySelector('.button-read-documentation');
 
 appButtonVideo.onmouseover = function () {
     appButtonVideo.classList.add('button-app-active');
-    appButtonVideo.style.backgroundColor = '#4C85FF';
-    appButtonVideo.style.color = 'white';
 }
 
 appButtonVideo.onmouseout = function () {
     appButtonVideo.classList.remove('button-app-active');
-    appButtonVideo.style.backgroundColor = '#F5F8FF';
-    appButtonVideo.style.color = '#ADB4D2';
 }
 
 appButtonFeatures.onmouseover = function () {
     appButtonFeatures.classList.add('button-app-active');
-    appButtonFeatures.style.backgroundColor = '#4C85FF';
-    appButtonFeatures.style.color = 'white';
 }
 
 appButtonFeatures.onmouseout = function () {
     appButtonFeatures.classList.remove('button-app-active');
-    appButtonFeatures.style.backgroundColor = '#F5F8FF';
-    appButtonFeatures.style.color = '#ADB4D2';
 }
 
 appButtonView.onmouseover = function () {
     appButtonView.classList.add('button-get-started-active');
-    appButtonView.classList.remove('button-get-started-disable');
-    appButtonView.style.backgroundColor = '#FFFFFF';
-    appButtonView.style.color = '#565F82';
 }
 
 appButtonView.onmouseout = function () {
     appButtonView.classList.remove('button-get-started-active');
-    appButtonView.classList.add('button-get-started-disable');
-    appButtonView.style.background = '#linear-gradient(229.32deg, #6888FF 12.77%, #5E80FF 73.11%)';
-    appButtonView.style.color = '#C7D3FF';
 }
 
 appButtonRead.onmouseover = function () {
     appButtonRead.classList.add('button-get-started-active');
-    appButtonRead.classList.remove('button-get-started-disable');
-    appButtonRead.style.backgroundColor = '#FFFFFF';
-    appButtonRead.style.color = '#565F82';
 }
 
 appButtonRead.onmouseout = function () {
     appButtonRead.classList.remove('button-get-started-active');
-    appButtonRead.classList.add('button-get-started-disable');
-    appButtonRead.style.background = '#linear-gradient(229.32deg, #6888FF 12.77%, #5E80FF 73.11%)';
-    appButtonRead.style.color = '#C7D3FF';
 }
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+var slideIndexNew = 1;
+showSlidesNew(slideIndexNew);
+
+// Next/previous controls
+function plusSlidesNew(n) {
+    showSlidesNew(slideIndexNew += n);
+}
+
+// Thumbnail image controls
+function currentSlideNew(n) {
+    showSlidesNew(slideIndexNew = n);
+}
+
+function showSlidesNew(n) {
+    var i;
+    var slidesNew = document.getElementsByClassName("my-slides-2");
+    var dotsNew = document.getElementsByClassName("dot-2");
+    if (n > slidesNew.length) {slideIndexNew = 1}
+    if (n < 1) {slideIndexNew = slidesNew.length}
+for (i = 0; i < slidesNew.length; i++) {
+    slidesNew[i].style.display = "none";
+}
+for (i = 0; i < dotsNew.length; i++) {
+    dotsNew[i].className = dotsNew[i].className.replace(" active", "");
+}
+slidesNew[slideIndexNew-1].style.display = "flex";
+dotsNew[slideIndexNew-1].className += " active";
+}
