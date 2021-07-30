9a8c106c07f12f9ffc2912a9c751156c8d73d7ca
loitez <87999189+loitez@users.noreply.github.com>
2021-07-30 15:49:14 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -124,3 +124,13 @@ var swiper = new Swiper(".mySwiper", {
 if ( window.history.replaceState ) {
     window.history.replaceState( null, null, window.location.href );
 }
+
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('.navbar-panel');
+const menuCloseBtn = document.querySelector('.close-menu');
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle('is-open');
+});
+menuCloseBtn.addEventListener("click", () => {
+    menu.classList.toggle('is-open');
+});
